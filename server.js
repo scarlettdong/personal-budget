43fcaf92b5bbe3ae0b7a9bcff5f7fc44033abc4f
scarlettdong <sdong5@uncc.edu>
2023-09-17 14:00:39 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget : [
-        {
-            title :'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350 
-        },
-        {
-            title: 'Groceries',
-            budget: 90
-        },
-    ]
-};
+// const budget = {
+//     myBudget : [
+//         {
+//             title :'Eat out',
+//             budget: 30
+//         },
+//         {
+//             title: 'Rent',
+//             budget: 350 
+//         },
+//         {
+//             title: 'Groceries',
+//             budget: 90
+//         },
+//     ]
+// };
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('budgetdata.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("file read error:", err);
+            res.status(500).send('Server error');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 app.listen(port, ()=>{
